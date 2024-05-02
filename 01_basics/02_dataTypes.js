0a86194f32cd11361d6107476456f32477fc2029
manojchokhar <145636770+manojchokhar@users.noreply.github.com>
2024-05-02 03:30:44 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // Treat all js code as newer version
+
+//alert(3 +3) // We are Using Node.js Not Browser
+
+console.log(3 + 3)
+
+console.log("Manoj")
+
+let name = "manoj"
+
+let age = 20;
+
+let isLoggedIn = true
+
+
+// DETATYPES =
+// Number =
+// bigint = biggest number
+// string = ""
+// boolean = true or false
+// null = standalone value
+// undefined =
+// symbol = unique
+
+
+// Object 
+
+console.log(typeof "manoj")
+
+console.log(typeof undefined) //output= undefined
+console.log(typeof null) //output= object
